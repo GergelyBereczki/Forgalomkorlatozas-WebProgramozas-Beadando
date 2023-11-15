774683228f3ba5c73acd6ba0c1317d7241263118
Bereczki Gergely <106348350+OldMcDonalds@users.noreply.github.com>
2023-11-15 18:56:31 +0100

    Add files via upload

--- controllers/regisztral.php
+++ controllers/regisztral.php
@@ -1,20 +1,18 @@
 <?php
 
-class Regisztral
-{
-	public function main(array $vars) {
-	// Betöltjük a nézetet
-	$view = new View_Loader($this->regisztral."_main");
+class Regisztral_Controller {
+	public string $baseName = 'beleptet';    //meghatározni, hogy melyik oldalon vagyunk
 
-	// Adatok beállítása a nézethez
-	$viewData = [
-		'uzenet' => 'Üdvözöljük a regisztrációnál!'
-		// Egyéb adatokat is adhatsz át a nézethez, amire szükséged van
-	];
-
-	// Átadjuk a nézetnek az adatokat
-	$view->assign('viewData', $viewData);
+	public function main(array $vars) {    // a router által továbbított paramétereket kapja
+		$regisztralModel = new Regisztral_Model;    //az osztályhoz tartozó modell
+		//a modellben beregisztráljuk a felhasználót
+		$retData = $regisztralModel->get_data($vars);
+		if ($retData['eredmeny'] == "ERROR")
+			$this->baseName = "belepes";
+		//betöltjük a nézetet
+		$view = new View_Loader($this->baseName . '_main');
+		//Átadjuk a lekérdezett adatokat a nézetnek
+		foreach ($retData as $name => $value)
+			$view->assign($name, $value);
+	}
 }
-}
-
-?>
