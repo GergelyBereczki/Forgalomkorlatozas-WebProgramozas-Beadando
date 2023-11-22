b08947d82e2c1fd0d89c2359e9a09be3f2b95a71
Bereczki Gergely <106348350+OldMcDonalds@users.noreply.github.com>
2023-11-22 20:23:54 +0100

    Update web2.sql

--- web2.sql
+++ web2.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `varosok`
 --
-CREATE DATABASE IF NOT EXISTS `web2` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `varosok` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `varosok`;
 
 -- --------------------------------------------------------
