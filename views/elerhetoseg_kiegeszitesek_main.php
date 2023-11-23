4fe173d6667d31d88e108e86bb2ceb09ae916c3f
Bagó Bence József <106177569+Sh4dyGh0st@users.noreply.github.com>
2023-11-23 21:29:29 +0100

    soapv2
    soapv2

--- views/elerhetoseg_kiegeszitesek_main.php
+++ views/elerhetoseg_kiegeszitesek_main.php
@@ -8,24 +8,67 @@ $options = array(
 try {
     $kliens = new SoapClient(null, $options);
 
-    $forgalomKorlatozasok = $kliens->getForgalomKorlatozasok();
+    $selectedMegnevezes = '';
+    $selectedMertek = '';
+    $selectedForgalomKorlatozas = '';
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $selectedMegnevezes = $_POST['megnevezes'];
+        $selectedMertek = $_POST['mertek'];
+        $selectedForgalomKorlatozas = $_POST['forgalomKorlatozas'];
+    }
+
     $megnevezesek = $kliens->getMegnevezes();
     $mertek = $kliens->getMertek();
+    $forgalomKorlatozasok = $kliens->getForgalomKorlatozasok($selectedMegnevezes, $selectedMertek);
 
-    echo "<h2>Forgalomkorlátozások:</h2>";
-    echo "<pre>";
-    print_r($forgalomKorlatozasok);
-    echo "</pre>";
+    echo "<form method='post'>";
 
     echo "<h2>Megnevezések:</h2>";
-    echo "<pre>";
-    print_r($megnevezesek);
-    echo "</pre>";
+    echo "<select name='megnevezes'>";
+    foreach ($megnevezesek as $kulcs => $ertek) {
+        $selected = ($kulcs == $selectedMegnevezes) ? 'selected' : '';
+        echo "<option value='$kulcs' $selected>$ertek</option>";
+    }
+    echo "</select>";
 
     echo "<h2>Mértékek:</h2>";
-    echo "<pre>";
-    print_r($mertek);
-    echo "</pre>";
+    echo "<select name='mertek'>";
+    foreach ($mertek as $kulcs => $ertek) {
+        $selected = ($kulcs == $selectedMertek) ? 'selected' : '';
+        echo "<option value='$kulcs' $selected>$ertek</option>";
+    }
+    echo "</select>";
+
+    echo "<h2>Forgalomkorlátozások:</h2>";
+    echo "<select name='forgalomKorlatozas'>";
+    foreach ($forgalomKorlatozasok as $kulcs => $ertek) {
+        $selected = ($kulcs == $selectedForgalomKorlatozas) ? 'selected' : '';
+        $megnevezes = $megnevezesek[$ertek['megnevezes']];
+        $mertekNev = $mertek[$ertek['mertek']];
+
+        echo "<option value='$kulcs' $selected>$megnevezes - $mertekNev</option>";
+    }
+    echo "</select>";
+
+    echo "<input type='submit' value='Kiválasztás'>";
+
+    echo "</form>";
+
+    // Kiválasztott forgalomkorlátozás adatainak megjelenítése
+    if (!empty($selectedForgalomKorlatozas)) {
+        $selectedData = $forgalomKorlatozasok[$selectedForgalomKorlatozas];
+        echo "<h2>Kiválasztott Forgalomkorlátozás:</h2>";
+        echo "Azonosító: $selectedForgalomKorlatozas<br>";
+        echo "Útszám: " . $selectedData['utszam'] . "<br>";
+        echo "Kezdet: " . $selectedData['kezdet'] . "<br>";
+        echo "Vég: " . $selectedData['veg'] . "<br>";
+        echo "Település: " . $selectedData['telepules'] . "<br>";
+        echo "Mettől: " . $selectedData['mettol'] . "<br>";
+        echo "Meddig: " . $selectedData['meddig'] . "<br>";
+        echo "Sebesség: " . $selectedData['sebesseg'] . "<br>";
+    }
+
 } catch (SoapFault $e) {
     var_dump($e);
 }
